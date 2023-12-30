0af11648e06479bb33693d5e66490c161ace6754
Danylo Tytarenko <87567245+Danyatitar@users.noreply.github.com>
2023-12-30 23:32:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,50 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// import * as cookieParser from 'cookie-parser';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: 'http://localhost:4200',
+//     credentials: true,
+//     exposedHeaders: ['Content-Disposition'],
+//   });
+//   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+//   app.use(cookieParser());
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const expressApp = express();
+
+async function bootstrap(expressInstance: express.Express) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
   app.enableCors({
-    origin: 'http://localhost:4200',
+    // Ensure that your CORS settings are configured correctly for your Vercel deployment.
+    origin: 'http://localhost:4200', // or specify origins you want to allow
     credentials: true,
     exposedHeaders: ['Content-Disposition'],
   });
+
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   app.use(cookieParser());
-  await app.listen(3000);
+
+  await app.init();
 }
-bootstrap();
+
+bootstrap(expressApp);
+
+export default expressApp;
